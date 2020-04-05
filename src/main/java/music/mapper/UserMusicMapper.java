8561e0bee3c1d1a8d56f52890e8bee4d06539f50
jhwu <1273163614@qq.com>
2020-04-05 16:08:46 +0800

    UserMusic

--- src/main/java/music/mapper/UserMusicMapper.java
+++ src/main/java/music/mapper/UserMusicMapper.java
@@ -1,7 +1,10 @@
 package music.mapper;
 
 import music.entity.UserMusic;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -18,17 +21,40 @@ public interface UserMusicMapper {
      *
      * @param userMusic
      */
+    @Insert("INSERT IGNORE INTO\n" +
+            "t_user_music(music_id,user_id)\n" +
+            "VALUES (#{musicId},#{userId})")
     void likeMusic(UserMusic userMusic);
     /**
      * 批量删除
      * @param idList
      * @return
      */
+    @Delete("<script>" +
+            "DELETE FROM t_user_music WHERE music_id IN" +
+            "<foreach collection=\"idList\" index=\"index\" item=\"item\" open=\"(\" separator=\",\" close=\")\">" +
+            "#{item}" +
+            "</foreach>" +
+            "</script>")
     int batchDelete(@Param("idList") List<Integer> idList);
     /**
      * 批量插入
      * @param userMusics
      * @return int
      */
+    @Insert("<script>" +
+            "INSERT INTO t_user_music VALUES" +
+            "<foreach collection=\"userMusics\" item=\"item\" index=\"index\" separator=\",\">" +
+            "(#{item.id},#{item.musicId},#{item.userId})" +
+            "</foreach>" +
+            "</script>")
     int batchInsert(@Param("userMusics") List<UserMusic> userMusics);
+
+    /**
+     * 查重
+     * @param userId musicId
+     * @return re
+     */
+    @Select("SELECT * FROM t_user_music WHERE user_id = #{userId} OR music_id = #{musicId}")
+    UserMusic re(@Param("userId") Integer userId, @Param("musicId") Integer musicId);
 }
--- src/main/java/music/service/UserMusicService.java
+++ src/main/java/music/service/UserMusicService.java
@@ -1,5 +1,9 @@
 package music.service;
 
+import music.entity.UserMusic;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 import music.entity.User;
 
 /**
@@ -10,5 +14,23 @@ import music.entity.User;
  * @Version 1.0
  **/
 public interface UserMusicService {
+    /**
+     *
+     * @param userMusic
+     */
+    void likeMusic(UserMusic userMusic);
+    /**
+     * 批量删除
+     * @param idList
+     * @return
+     */
+    int batchDelete(@Param("idList") List<Integer> idList);
+    /**
+     * 批量插入
+     * @param userMusics
+     * @return int
+     */
+    int batchInsert(@Param("userMusics") List<UserMusic> userMusics);
 
+    UserMusic re(Integer userId,Integer musicId);
 }
